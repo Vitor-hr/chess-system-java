dc4ae931f1d948456a04f3dc1af396bf9ff4939f
Vitor-hr <117821814+Vitor-hr@users.noreply.github.com>
2024-07-07 19:31:18 -0300

    Switching player each turn

--- untitled/src/application/UI.java
+++ untitled/src/application/UI.java
@@ -1,4 +1,5 @@
 package application;
+import chess.ChessMatch;
 import chess.ChessPosition;
 import chess.Color;
 import chess.ChessPiece;
@@ -52,6 +53,14 @@ public class UI {
             System.out.println();
         }
         System.out.println("  a b c d e f g h");
+    }
+
+    public static void printMatch(ChessMatch chessMatch){
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn : " + chessMatch.getTurn());
+        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+
     }
 
     public static void printBoard(ChessPiece[][] pieces, boolean [][] possibleMoves){
