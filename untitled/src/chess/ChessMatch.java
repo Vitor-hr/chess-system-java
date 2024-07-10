0a91a6ef9a62701b1bb68d23149dd4285691325d
Vitor-hr <117821814+Vitor-hr@users.noreply.github.com>
2024-07-10 02:04:18 -0300

    bugfix promotion

--- untitled/src/chess/ChessMatch.java
+++ untitled/src/chess/ChessMatch.java
@@ -5,11 +5,9 @@ import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.*;
 
-import javax.xml.transform.Source;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.security.InvalidParameterException;
 
 public class ChessMatch {
     private Board board;
@@ -115,8 +113,9 @@ public class ChessMatch {
             throw new IllegalStateException("There is no piece to be promoted");
         }
         if (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
-            throw new InvalidParameterException("Invalid type for promotion");
+            return promoted;
         }
+
 
         Position pos = promoted.getChessPosition().toPosition();
         Piece p = board.removePiece(pos);
